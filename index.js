d7b4f00b145345834b9ad3589d7f53abcc5dd416
maziar <maziar.gh.00@gmail,com>
2023-05-02 15:25:23 +0330

    [fix]

--- index.js
+++ index.js
@@ -9,6 +9,10 @@ var fs = require('fs');
 var http = require('http');
 var https = require('https');
 
+const MongoClient = require('mongodb').MongoClient;
+const url = 'mongodb://localhost:27017'; // Replace with your MongoDB connection URL
+const dbName = 'pushnotifications'; // Replace with your database name
+
 var pk  = fs.readFileSync(process.env.PRIVATE_KEY, 'utf8');
 var cr = fs.readFileSync(process.env.FULL_CHAIN, 'utf8');
 var credentials = {key: pk, cert: cr};
@@ -24,60 +28,79 @@ const privateVapidKey = process.env.PRIVATE_VAPID_KEY;
 
 webPush.setVapidDetails('mailto:test@example.com', publicVapidKey, privateVapidKey);
 
-// Subscribe route
+// Establish a connection to MongoDB
+MongoClient.connect(url, { useUnifiedTopology: true }, (err, client) => {
+  if (err) {
+    console.error('Error connecting to MongoDB:', err);
+    return;
+  }
 
-app.post('/subscribe', (req, res) => {
-  const subscription = req.body
+  console.log('Connected to MongoDB');
 
-  res.status(201).json({});
+  // Specify the database and collection
+  const db = client.db(dbName);
+  const subscriptionsCollection = db.collection('subscriptions');
 
-  // create payload
-  const payload = JSON.stringify({
-    title: 'Push notifications with Service Workers',
+  // Route to handle subscription requests
+  app.post('/subscribe', (req, res) => {
+    const subscription = req.body;
+
+    // Save the subscription details in the database
+    subscriptionsCollection.insertOne(subscription, (err, result) => {
+      if (err) {
+        console.error('Error saving subscription:', err);
+        res.status(500).json({ error: 'Internal server error' });
+        return;
+      }
+
+      res.status(201).json({ message: 'Subscription successful' });
+    });
   });
 
-  webPush.sendNotification(subscription, payload)
-    .catch(error => console.error(error));
-});
 
-// (E) SEND TEST PUSH NOTIFICATION
-app.post("/mypush", (req, res) => {
-  res.status(201).json({}); // reply with 201 (created)
+    // Route to send push notifications
+  app.post('/send-notification', (req, res) => {
+    const notificationPayload = {
+      title: 'New Notification',
+      body: 'This is a push notification sent from your server',
+    };
 
-  webPush.sendNotification(subscription, JSON.stringify({
-    title: "Welcome!",
-    body: "Yes, it works!",
-    icon: "https://cdn-icons-png.flaticon.com/512/3135/3135715.png",
-    image: "https://www.simplilearn.com/ice9/free_resources_article_thumb/what_is_image_Processing.jpg",
-    badge:"https://cdn-icons-png.flaticon.com/512/3135/3135715.png",
-    actions: [
-        {
-          action: 'link',
-          type: 'button',
-          title: 'show',
-          icon: 'https://cdn-icons-png.flaticon.com/512/3135/3135715.png',
-        },
-      ],
-  }))
-    .catch(error => console.error(error));
-});
+    // Fetch subscription details from the database
+    subscriptionsCollection.find().toArray((err, subscriptions) => {
+      if (err) {
+        console.error('Error fetching subscriptions:', err);
+        res.status(500).json({ error: 'Internal server error' });
+        return;
+      }
 
+      // Send the push notifications to all subscriptions
+      subscriptions.forEach(subscription => {
+        webpush
+          .sendNotification(subscription, JSON.stringify(notificationPayload))
+          .catch(error => {
+            console.error('Error sending notification:', error);
+          });
+      });
 
-
-
-
-var httpServer = http.createServer(app);
-var httpsServer = https.createServer(credentials, app);
-
-// app.set('port', process.env.PORT_HTTP || 5000);
-// const server = app.listen(app.get('port'), () => {
-//   console.log(`Express running → PORT ${server.address().port}`);
-// });
+      res.status(200).json({ message: 'Notification sent' });
+    });
+  });
 
 
-httpServer.listen(process.env.PORT_HTTP || 8080, () => {
-  console.log("httpServer is runing at port 8080");
-});
-httpsServer.listen(process.env.PORT_HTTPS || 5000, () => {
-  console.log("httpsServer is runing at port 8443");
+  var httpServer = http.createServer(app);
+  var httpsServer = https.createServer(credentials, app);
+
+  // app.set('port', process.env.PORT_HTTP || 5000);
+  // const server = app.listen(app.get('port'), () => {
+  //   console.log(`Express running → PORT ${server.address().port}`);
+  // });
+
+
+  httpServer.listen(process.env.PORT_HTTP || 8080, () => {
+    console.log("httpServer is runing at port 8080");
+  });
+  httpsServer.listen(process.env.PORT_HTTPS || 5000, () => {
+    console.log("httpsServer is runing at port 8443");
+  });
+
 });
