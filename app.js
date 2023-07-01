8863ae9f26ba3383b5804caa508731be6acca8ac
maziar <maziar.gh.00@gmail,com>
2023-07-01 16:39:13 +0330

    [update] disable cors

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ require('dotenv').config({ path: 'variables.env' });
 const path = require('path');
 
 const express = require('express');
+const cors = require('cors');
 const pushRouter = require('./routes/push.route');
 
 const bodyParser = require('body-parser');
@@ -10,6 +11,8 @@ const bodyParser = require('body-parser');
 const app = express();
 app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, 'client')));
+app.use(cors());
+
 //app.use(express.urlencoded({ extended: true }));
 
 app.use('/api/', pushRouter);
