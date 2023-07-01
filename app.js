a6de9707358270a4f90fda05486879ef97cdac9f
maziar <maziar.gh.00@gmail,com>
2023-07-01 16:51:59 +0330

    [fix] prev commit

--- app.js
+++ app.js
@@ -9,9 +9,18 @@ const pushRouter = require('./routes/push.route');
 const bodyParser = require('body-parser');
 
 const app = express();
+
+const corsOptions = {
+    origin: '*',
+    methods: [],
+    allowedHeaders: [],
+    exposedHeaders: [],
+    credentials: true
+};
+
+app.use(cors(corsOptions));
 app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, 'client')));
-app.use(cors());
 
 //app.use(express.urlencoded({ extended: true }));
 
